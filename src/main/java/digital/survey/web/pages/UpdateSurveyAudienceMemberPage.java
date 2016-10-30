3f131fc72c9687d39f906514e5efe1ac1b269076
Marcus Portmann <marcus@mmp.guru>
2016-10-30 12:16:03 +0200

    Updates

--- src/main/java/digital/survey/web/pages/UpdateSurveyAudienceMemberPage.java
+++ src/main/java/digital/survey/web/pages/UpdateSurveyAudienceMemberPage.java
@@ -1,11 +1,122 @@
+/*
+ * Copyright 2016 Marcus Portmann
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package digital.survey.web.pages;
 
+//~--- non-JDK imports --------------------------------------------------------
+
+import digital.survey.model.ISurveyService;
+import digital.survey.model.SurveyAudienceMember;
+import digital.survey.web.SurveySecurity;
+import digital.survey.web.components.SurveyAudienceMemberInputPanel;
+import guru.mmp.application.web.WebApplicationException;
+import guru.mmp.application.web.pages.WebPageSecurity;
+import guru.mmp.application.web.template.pages.TemplateWebPage;
+import org.apache.wicket.PageReference;
+import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.model.Model;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import java.util.UUID;
+
+//~--- JDK imports ------------------------------------------------------------
+
 /**
- * Created by marcus on 2016/10/28.
+ * The <code>UpdateSurveyAudienceMemberPage</code> class implements the
+ * "Add Survey Audience Member" page for the web application.
+ *
+ * @author Marcus Portmann
  */
-public class UpdateSurveyAudienceMemberPage
+@SuppressWarnings("CdiManagedBeanInconsistencyInspection")
+@WebPageSecurity(SurveySecurity.FUNCTION_CODE_SURVEY_ADMINISTRATION)
+public class UpdateSurveyAudienceMemberPage extends TemplateWebPage
 {
+  /* Logger */
+  private static final Logger logger = LoggerFactory.getLogger(UpdateSurveyAudienceMemberPage.class);
+  private static final long serialVersionUID = 1000000;
+
+  /* Survey Service */
+  @Inject
+  private ISurveyService surveyService;
+
+  /**
+   * Constructs a new <code>UpdateSurveyAudienceMemberPage</code>.
+   *
+   * @param previousPage     the previous page
+   * @param surveyAudienceId the Universally Unique Identifier (UUID) used to uniquely identify the
+   *                         survey audience the survey audience member is associated with
+   */
+  public UpdateSurveyAudienceMemberPage(PageReference previousPage, UUID surveyAudienceId)
+  {
+    super("Add Audience Member");
+
+    try
+    {
+      Form<SurveyAudienceMember> addForm = new Form<>("addForm", new CompoundPropertyModel<>(new Model<>(
+        new SurveyAudienceMember(UUID.randomUUID(), surveyAudienceId, "", "", ""))));
+
+      addForm.add(new SurveyAudienceMemberInputPanel("surveyAudienceMember"));
+
+      // The "addButton" button
+      Button addButton = new Button("addButton")
+      {
+        private static final long serialVersionUID = 1000000;
+
+        @Override
+        public void onSubmit()
+        {
+          try
+          {
+            surveyService.saveSurveyAudienceMember(addForm.getModelObject());
+
+            setResponsePage(previousPage.getPage());
+          }
+          catch (Throwable e)
+          {
+            logger.error("Failed to add the survey audience: " + e.getMessage(), e);
+            UpdateSurveyAudienceMemberPage.this.error("Failed to add the survey audience");
+          }
+        }
+      };
+      addButton.setDefaultFormProcessing(true);
+      addForm.add(addButton);
+
+      // The "cancelButton" button
+      Button cancelButton = new Button("cancelButton")
+      {
+        private static final long serialVersionUID = 1000000;
+
+        @Override
+        public void onSubmit()
+        {
+          setResponsePage(previousPage.getPage());
+        }
+      };
+      cancelButton.setDefaultFormProcessing(false);
+      addForm.add(cancelButton);
+
+      add(addForm);
+    }
+    catch (Throwable e)
+    {
+      throw new WebApplicationException("Failed to initialise the UpdateSurveyAudienceMemberPage", e);
+    }
+  }
 }
-
-
-START HERE!!!!
