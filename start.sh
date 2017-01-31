dbbbd5d3502b08d8dbbc081a2572798fa1eb9399
Marcus Portmann <marcus@mmp.guru>
2017-01-31 14:19:35 +0200

    Updates

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-java -jar /root/src/survey/target/survey-swarm.jar --dbConnectionUrl=jdbc:postgresql://survey.ckfokthwz6ut.eu-west-1.rds.amazonaws.com:5432/survey --dbUsername=survey --dbPassword=GSPQzQ3EL95JCnaz
+java -jar target/survey-swarm.jar --port=80 --dbConnectionUrl=jdbc:postgresql://survey.ckfokthwz6ut.eu-west-1.rds.amazonaws.com:5432/survey --dbUsername=survey --dbPassword=GSPQzQ3EL95JCnaz
 
